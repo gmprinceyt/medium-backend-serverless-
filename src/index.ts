9e1486d9fab33fb4b2912980433b313c9fdfd6ef
PRINCE KUAMR <gmprince420@gmail.com>
2025-10-04 15:22:10 +0530

    add logic in User

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,54 @@ import { Hono } from "hono";
 import { PrismaClient } from "./generated/prisma/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { HTTPException } from "hono/http-exception";
+import bcrypt from "bcryptjs";
+import { decode, sign, verify } from "hono/jwt";
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
+    JWT_SECRET: string;
   };
 }>();
 
 // POST /api/v1/user/signup
 app.post("/api/v1/user/signup", async (c) => {
-  
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
   try {
-    const prisma = new PrismaClient({
-      datasourceUrl: c.env.DATABASE_URL,
-    }).$extends(withAccelerate());
-    const user  = await prisma.user.create({
+    const { name, email, password } = await c.req.json<{
+      name: string | null;
+      email: string;
+      password: string;
+    }>();
+
+    if (!email || !password) {
+      throw new HTTPException(411, {
+        message: "email, password are required ",
+      });
+    }
+
+    const hashPass = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
       data: {
-        name: "Vikash kumar",
-        email: "Vikash@gmail.com",
-        password: "PRINCE123"
-      }
-    })
-    c.status(200);
-    return c.json(user)
+        name,
+        email,
+        password: hashPass,
+      },
+      select: {
+        id: true,
+        email: true,
+      },
+    });
+
+    const token = await sign(user, c.env.JWT_SECRET);
+    c.status(201);
+    return c.json({
+      message: "User Create Successfully ",
+      token,
+    });
   } catch (error) {
     if (error instanceof Error) {
       throw new HTTPException(500, { message: error.message });
@@ -35,11 +60,44 @@ app.post("/api/v1/user/signup", async (c) => {
 });
 
 // POST /api/v1/user/signin
-app.post("/api/v1/user/signin", (c) => {
-  c.status(200);
-  c.json({
-    message: "Successfully Signin",
-  });
+app.post("/api/v1/user/signin", async (c) => {
+  const prsima = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  try {
+    const {email, password}  = await c.req.json<{email: string, password: string}>();
+
+    if (!email || !password){
+      throw new HTTPException(411, {message: "email, password field are required"})
+    };
+
+    const user  = await prsima.user.findUnique({
+      where: {email}, 
+    })
+
+    if (!user ){
+      throw new HTTPException(404 , {message: "User not Found"});
+    };
+
+    const isMatchPass = await bcrypt.compare(password, user.password);
+
+    if( !isMatchPass) {
+      throw new HTTPException(400,{message: "Password Does'nt Matched"});
+    };
+
+    const token = await sign({id: user.id, email: user.email}, c.env.JWT_SECRET);
+    c.status(200);
+    return c.json({
+      message: "User login Success",
+      token
+    })
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new HTTPException(400, { message: error.message });
+    } else {
+      throw new HTTPException(500, { message: "internal server error" });
+    }
+  }
 });
 
 // POST /api/v1/blog
